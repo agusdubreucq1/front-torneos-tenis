e35d22964e3572818a55e79860d612d1e21d63fb
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-01-12 09:15:25 -0300

    dibujo del cuadro

--- src/components/CardPartidoVacio.tsx
+++ src/components/CardPartidoVacio.tsx
@@ -3,15 +3,18 @@ import styles from "../styles/drawTournament.module.css"
 
 const CardPartidoVacio: React.FC = () => {
     return (
-        <div className={styles.partido}>
-            <div className={styles.jugadores}>
-                <p>-</p>
-                <p>-</p>
-            </div>
-            <div className={styles.resultado}>
-                <p>-</p>
+        <div className={styles.container_partido}>
+            <div className={styles.partido}>
+                <div className={styles.jugadores}>
+                    <p>-</p>
+                    <p>-</p>
+                </div>
+                <div className={styles.resultado}>
+                    <p>-</p>
+                </div>
             </div>
         </div>
+
     )
 }
 
