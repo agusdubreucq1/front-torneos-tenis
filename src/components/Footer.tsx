655ac7046194560252ff9703303746434084c998
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-02-03 09:30:11 -0300

    fix links footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <a href='https://github.com/agusdubreucq1?tab=repositories'><img src={github}></img></a>
-                <a href='https://agusdubreucq.com/'><img src={web}></img></a>
+                <a href='https://github.com/agusdubreucq1?tab=repositories' target='_blank'><img src={github}></img></a>
+                <a href='https://agusdubreucq.com/' target='_blank'><img src={web}></img></a>
             </div>
         </footer>
     );
